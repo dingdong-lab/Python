8eb7e54bf742a3bbdda7f6c60bba10dbee1b60fa
dingdong-lab <dingdong1-2@wp.pl>
2020-04-08 12:04:26 +0200

    5 commit

---
+++ cwiczenie5.py
@@ -0,0 +1,11 @@
+#zad 1
+class materiał:
+    def __init__(self, rodzaj, dlugosc, szerokosc):
+        self.rodzaj = rodzaj 
+        self.dlugosc = dlugosc 
+        self.szerokosc = szerokosc 
+
+    def wyswietl_nazwe(self):
+        print(self.rodzaj)
+
+    

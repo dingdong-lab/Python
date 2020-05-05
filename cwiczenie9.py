75af0de0a15bcd3e9868d177881b67b68520274a
dingdong-lab <dingdong1-2@wp.pl>
2020-05-05 15:53:00 +0200

    dziewiaty commit

--- cwiczenie9.py
+++ cwiczenie9.py
@@ -34,10 +34,20 @@ grupa4 = dane2.copy()
 grupa4['class'] = grupa4['class'].astype('|S')
 grupa4.drop('petal length in cm', axis=1, inplace=True)
 grupa4.drop('petal width in cm', axis=1, inplace=True)
+lista1 = []
+for x in grupa4['class']:
+    if x == b'Iris-setosa':
+        lista1.append('red')
+    elif x == b'Iris-versicolor':
+        lista1.append('blue')
+    else:
+        lista1.append('black')
+grupa4['class'] = lista1
 
-grupa4[grupa4['class']==b'Iris-setosa'].plot.scatter(x='sepal length in cm', y='sepal width in cm', c='black', label='Iris Setosa')
-grupa4[grupa4['class']==b'Iris-versicolor'].plot.scatter(x='sepal length in cm', y='sepal width in cm', c='red', label='Iris Versicolour')
-grupa4[grupa4['class']==b'Iris-virginica'].plot.scatter(x='sepal length in cm', y='sepal width in cm', c='blue', label='Iris Virginica')
+#grupa4[grupa4['class']==b'Iris-setosa'].plot.scatter(x='sepal length in cm', y='sepal width in cm', c='black', label='Iris Setosa')
+#grupa4[grupa4['class']==b'Iris-versicolor'].plot.scatter(x='sepal length in cm', y='sepal width in cm', c='red', label='Iris Versicolour')
+#grupa4[grupa4['class']==b'Iris-virginica'].plot.scatter(x='sepal length in cm', y='sepal width in cm', c='blue', label='Iris Virginica')
+grupa4.plot.scatter(x='sepal length in cm', y='sepal width in cm', c='class')
 
 plt.show()
 
